bca2695ab98ae5c1dd9daabb518aae968ae35d00
하승우 <winwoo4946@neowiz.com>
2023-08-09 10:18:05 +0900

    서버 react deploy경로 수정

--- mini/server.js
+++ mini/server.js
@@ -104,7 +104,7 @@ const apiRegister = () => {
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './build/index.html'));
+        res.sendFile(path.join(__dirname, './deploy/index.html'));
     });
 }
 
